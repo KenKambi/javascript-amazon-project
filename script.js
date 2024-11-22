53ed75143eca365cffe20e730dfd4707cd3ec05b
Ken Kambi <kenkambi43@gmail.com>
2024-11-22 20:48:57 +0300

    added html to script.js using objects inside an array

---
+++ script.js
@@ -0,0 +1,27 @@
+const products = [{
+    image: `images/products/athletic-cotton-socks-6-pairs.jpg`,
+    name: `Black and Gray Athletic Cotton Socks - 6 Pairs`,
+    rating:{
+        star: 4.5,
+        count: 87
+    },
+    priceCent: 1090
+},
+{
+    image: `images/products/intermediate-composite-basketball.jpg`,
+    name: `Intermediate Size Basketball`,
+    rating:{
+        star: 4.0,
+        count: 127
+    },
+    priceCent: 2095
+},
+{
+    image: `images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg`,
+    name: `Adults Plain Cotton T-Shirt - 2 Pack`,
+    rating:{
+        star: 4.5,
+        count: 56
+    },
+    priceCent: 799
+}];
